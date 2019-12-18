c9d11551384d06d889367f8516f1e6bbf1154883
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-12-18 13:48:06 +0300

    Remove DB.Internal module

--- backend/src/DB.hs
+++ backend/src/DB.hs
@@ -1,14 +1,36 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 module DB (
   module Database.Persist,
-  connectDb,
+  runDb,
   updateBy,
-  runDb
+  connectDb
   ) where
 
 import Database.Persist
+import Database.Persist.Sql (SqlPersistT, runMigration, runSqlPool, ConnectionPool)
+import Database.Persist.Postgresql (ConnectionString, createPostgresqlPool)
+import Control.Monad.Reader (ReaderT, MonadReader, asks)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Logger (runStderrLoggingT)
 
-import DB.Internal (
-  connectDb,
-  updateBy,
-  runDb
-  )
+import Config
+
+connectDb :: ConnectionString -> IO ConnectionPool
+connectDb connectionString = runStderrLoggingT $ createPostgresqlPool connectionString 1
+
+runDb :: (MonadReader Config m, MonadIO m) => SqlPersistT IO b -> m b
+runDb query = do
+  pool <- asks configPool
+  liftIO $ runSqlPool query pool
+
+updateBy :: (MonadIO m, PersistUniqueRead backend, PersistEntity record, PersistStoreWrite backend, PersistEntityBackend record ~ BaseBackend backend)
+         => Unique record
+         -> [Update record]
+         -> ReaderT backend m ()
+updateBy unique updates = do
+  mExisting <- getBy unique
+  case mExisting of
+    Just (Entity key _) -> update key updates
+    Nothing -> return ()
