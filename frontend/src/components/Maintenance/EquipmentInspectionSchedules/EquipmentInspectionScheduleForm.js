4ccc14b9179216e6edc0a05db81bd15abff9212e
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-12-23 22:07:10 +0300

    Add EquipmentInspectionScheduleForm

--- frontend/src/components/Maintenance/EquipmentInspectionSchedules/EquipmentInspectionScheduleForm.js
+++ frontend/src/components/Maintenance/EquipmentInspectionSchedules/EquipmentInspectionScheduleForm.js
@@ -1,59 +1,54 @@
 import React, { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { postEquipmentInstance } from '../../../actions/maintenance/equipmentInstances';
-import { fetchEquipment } from '../../../actions/maintenance/equipment';
-import { fetchUnits } from '../../../actions/admin/units';
+import { postEquipmentInspectionSchedule } from '../../../actions/maintenance/equipmentInspectionSchedules';
+import { fetchEquipmentInstances } from '../../../actions/maintenance/equipmentInstances';
+import { fetchEmployees } from '../../../actions/admin/employees';
 import useForm from 'react-hook-form';
 
-export default function EquipmentInstanceForm() {
+export default function EquipmentInspectionScheduleForm() {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
-  const equipment = useSelector(state => state.equipment);
-  const units = useSelector(state => state.units);
+  const equipmentInstances = useSelector(state => state.equipmentInstances);
+  const employees = useSelector(state => state.employees);
   useEffect(() => {
-    dispatch(fetchEquipment());
-    dispatch(fetchUnits());
+    dispatch(fetchEquipmentInstances());
+    dispatch(fetchEmployees());
   }, [dispatch]);
   const onSubmit = values => {
-    values.model = Number(values.model);
-    values.unit = Number(values.unit);
-    values.installedAt = new Date(values.installedAt).toISOString();
+    values.equipmentInstance = Number(values.equipmentInstance);
+    values.employee = Number(values.employee);
+    values.dateTime = new Date(values.installedAt).toISOString();
+    values.completed = false;
     console.log(values);
-    dispatch(postEquipmentInstance(values))
+    dispatch(postEquipmentInspectionSchedule(values))
   };
-  const modelOptions = equipment.map(
-    equipment => <option key={equipment.id} value={equipment.id}>{equipment.name}</option>
+  const equipmentInstancesOptions = equipmentInstances.map(
+    equipmentInstance => <option key={equipmentInstance} value={equipmentInstance.id}>{equipmentInstance.id}</option>
   );
-  const modelSelect = (
-    <select name="model" ref={register}>
-      {modelOptions}
+  const equipmentInstanceSelect = (
+    <select name="equipmentInstance" ref={register}>
+      {equipmentInstancesOptions}
     </select>
   );
-  const unitOptions = units.map(
-    unit => <option key={unit.id} value={unit.id}>{unit.name}</option>
+  const employeeOptions = employees.map(
+    employee => <option key={employee.id} value={employee.id}>{employee.name}</option>
   );
-  const unitSelect = (
-    <select name="unit" ref={register}>
-      {unitOptions}
+  const employeeSelect = (
+    <select name="employee" ref={register}>
+      {employeeOptions}
     </select>
   );
   return (
     <Fragment>
-      <h2>Add equipment instance</h2>
+      <h2>Add equipment inspectionSchedule</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Comment: </label>
-        <input name="comment" ref={register}></input><br/>
-        <label>Location: </label>
-        <input name="location" ref={register}></input><br/>
-        <label>Operational: </label>
-        <input name="operational" type="checkbox" ref={register}></input><br/>
-        <label>InstalledAt: </label>
+        <label>DateTime: </label>
         <input name="installedAt" type="datetime-local" ref={register}></input><br/>
-        <label>Model: </label>
-        {modelSelect}<br/>
-        <label>Unit: </label>
-        {unitSelect}<br/>
-        <button>Add EquipmentInstance</button>
+        <label>EquipmentInstance: </label>
+        {equipmentInstanceSelect}<br/>
+        <label>Employee: </label>
+        {employeeSelect}<br/>
+        <button>Add EquipmentInspectionSchedule</button>
       </form>
     </Fragment>
   );
