988c45acdadd574589d4813d038b59ec9a6c341f
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-12-23 17:36:29 +0300

    Use <select> for selecting employee unit

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -1,22 +1,36 @@
-import React, { Fragment } from 'react';
-import { useDispatch } from 'react-redux';
-import { postEmployee } from '../actions/employees';  
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { postEmployee } from '../actions/employees';
+import { fetchUnits } from '../actions/units';
 import useForm from 'react-hook-form';
 
 export default function EmployeeForm() {
   const { register, handleSubmit } = useForm();
   const dispatch = useDispatch();
+  const units = useSelector(state => state.units);
+  useEffect(() => {
+    dispatch(fetchUnits());
+  }, [dispatch]);
   const onSubmit = values => {
+    console.log(values);
     values.unit = Number(values.unit);
     dispatch(postEmployee(values))
   };
+  const options = units.map(
+    unit => <option key={unit.id} value={unit.id}>{unit.name}</option>
+  );
+  const select = (
+    <select name="unit" ref={register}>
+      {options}
+    </select>
+  );
   return (
     <Fragment>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name: </label>
         <input name="name" ref={register}></input><br/>
         <label>Unit: </label>
-        <input type="number" name="unit" ref={register}></input><br/>
+        {select}<br/>
         <button>Add Employee</button>
       </form>
     </Fragment>
