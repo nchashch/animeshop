d7c6ea678bc7c9330dfe0a019695803a42819477
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-12-23 18:21:53 +0300

    Add ManufacturerForm

--- frontend/src/components/Stock/Manufacturers/index.js
+++ frontend/src/components/Stock/Manufacturers/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
+import ManufacturerForm from './ManufacturerForm';
 import ManufacturersList from './ManufacturersList';
 
 export default function Manufacturers() {
   return (
     <Fragment>
+      <ManufacturerForm/>
       <ManufacturersList/>
     </Fragment>
   );
